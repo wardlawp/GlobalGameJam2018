19e7669a183afd79ada54a9e179929ba598b7fed
Philip Wardlaw <wardlawp@tcd.ie>
2018-01-26 00:54:45 -0800

    Add test assets - so we can check this solution on other machines

---
+++ Assets/Test.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 input = new Vector3();
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+            input.y += 1;
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+            input.y -= 1;
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            input.x += 1;
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            input.x -= 1;
+
+        transform.Translate(input);
+    }
+}
